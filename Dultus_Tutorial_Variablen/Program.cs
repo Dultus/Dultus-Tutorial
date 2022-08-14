3e4d8159c9346fc0c45aa38c76a1ef353cb21f40
Dultus <Sven.Kribitz@Outlook.de>
2022-08-14 20:37:07 +0200

    Projektdateien hinzufügen.

---
+++ Dultus_Tutorial_Variablen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dultus_Tutorial_Variablen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
